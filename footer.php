7f2d8797dc80f3bc96566cc69ff04745113dc214
MalexT <aleksa.tedi.markovic@gmail.com>
2021-02-10 10:57:03 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4">
+                <div class="copyright">2019 © Studentska služba FON-a. Sva prava zadržana.</div>
+            </div>
+            <div class="col-md-4 col-sm-4">
+                
+            </div>
+            <div class="col-md-4 col-sm-4 text-right">
+                <p class="powered">Dizajn: <a href="#">UN</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+
---
+++ meni.php
@@ -0,0 +1,32 @@
+<div class="header header-mobi-ext">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col-sm-2">
+                <a class="scroll site-logo" href="#promo-block"><img src="assets/onepage/img/fonlogo.png" alt="Logo FONa"></a>
+            </div>
+            <a href="javascript:void(0);" class="mobi-toggler"><i class="fa fa-bars"></i></a>
+            <div class="col-md-10 pull-right">
+                <ul class="header-navigation">
+                    <li><a href="index.php">Pretraga prijava</a></li>
+                    <li><a href="unesiPrijavu.php">Nova prijava</a></li>
+                    <li><a href="pretragaPoStudentu.php">Pretraga po studentu</a></li>
+                    <li><a href="unesiPredmet.php">Novi predmet</a></li>
+					<?php if($_SESSION['sluzbenik']->proveriSluzbenika()){
+                      ?>
+                      <li><a href="scrnsvr.php">Screen</a></li>
+					  
+                      <?php
+                    } ?>
+                    <?php if($_SESSION['sluzbenik']->proveriAdministratora()){
+                      ?>
+                      <li><a href="adminStrane.php">Administracija</a></li>
+					  <li><a href="noviSluzbenik.php">Novi službenik</a></li>
+                      <?php
+                    } ?>
+                    <li><a href="fajlovi.php">Dokumenti</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
